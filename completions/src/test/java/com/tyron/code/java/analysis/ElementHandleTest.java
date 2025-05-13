aa816f6ba3b707d16b497b999fc6d60c9bbe7328
icy-person <kar.fars.1388@gmail.com>
2025-05-13 16:39:00 +0330

    Update ElementHandleTest.java

--- completions/src/test/java/com/tyron/code/java/analysis/ElementHandleTest.java
+++ completions/src/test/java/com/tyron/code/java/analysis/ElementHandleTest.java
@@ -20,6 +20,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
